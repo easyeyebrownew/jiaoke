7f4e5284af1c3f1cd38016a2196e5084884611de
Wayne_PC <easyeyebrow@outlook.com>
2016-04-09 20:46:58 +0800

    Add 4 Foms

---
+++ JKJK/NetworkForm.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace JKJK
+{
+    /// <summary>
+    /// Interaction logic for NetworkForm.xaml
+    /// </summary>
+    public partial class NetworkForm : Window
+    {
+        public NetworkForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
